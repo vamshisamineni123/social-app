4bd691da4c97fee5a2aca38c0da8b329dbf7f652
vamshisamineni123 <vamshisamineni3@gmail.com>
2023-06-11 00:59:28 +0530

    2nd commit

---
+++ src/components/Online/Online.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Online.css'
+export default function Online() {
+    return (
+       
+            <li className='rightBarFriend'>
+                <div className='rightBarProfileContainer'>
+                    <img className="imge" src="assets/person/3.jpeg" />
+                    <span className='rightBarOnline'> </span>
+                </div>
+                <span className='rightBarUserName'>Vamshi</span>
+            </li>
+        
+            )
+}
--- src/components/RightBar/RightBar.jsx
+++ src/components/RightBar/RightBar.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import './RightBar.css'
+import Online from '../Online/Online';
 function RightBar() {
     return (
         <div className='rightBar'>
             <div className='rightBarWrapper'>
                 <div className='birthdayContainer'>
-                    <img scr="https://wallpapercave.com/wp/wc1714562.jpg" alt="birthdaypic"/>
+                    <img className='birthdayImg' src="/assets/gift.png" alt="birthdaypic"/>
                     <span className='birthdayText'>Vamsi and 3 others friends have birthday today</span>
                 </div>
-
-
+                <img className='rightBarAd' src="/assets/ad.png" alt="alt"/>
+                <h1 className='rightBarTitle'>Online Friends</h1>
+                <ul className='rightBarFriendList'>
+                  <Online />
+                </ul>
             </div>
         </div>
     )
