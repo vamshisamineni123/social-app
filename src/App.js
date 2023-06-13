ca884a6dc19ebf493c3244d51b9eeee0071728e3
Vamshi Samineni (MAQ LLC) <v-vsamineni@microsoft.com>
2023-06-13 17:50:12 +0530

    login and register commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@
 import './App.css';
 import SideBar from './components/SideBar/SideBar';
 import Home from './pages/Home';
+import Login from './pages/Login/Login';
 import Profile from './pages/Profile/Profile';
+import Register from './pages/Register/Register';
 function App() {
   return (
     <div className="App"> 
-     <Profile/>
+     {/* <Profile/> */}
+     {/* <Login/> */}
+     <Register/>
     </div>
   );
 }
