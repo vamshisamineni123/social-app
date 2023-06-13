fb1a9ab7439abdce9996de2e0f7557754b958921
vamshisamineni123 <vamshisamineni3@gmail.com>
2023-06-13 07:50:46 +0530

    second for the day

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@
 import './App.css';
 import SideBar from './components/SideBar/SideBar';
 import Home from './pages/Home';
+import Profile from './pages/Profile/Profile';
 function App() {
   return (
     <div className="App"> 
-     <Home/>
+     <Profile/>
     </div>
   );
 }
---
+++ src/pages/Profile/Profile.jsx
@@ -0,0 +1,40 @@
+import "./Profile.css";
+import Topbar from "../../components/TopBar/TopBar";
+import Sidebar from "../../components/SideBar/SideBar";
+import Feed from "../../components/Feed/Feed";
+import Rightbar from "../../components/RightBar/RightBar";
+
+export default function Profile() {
+    return (
+        <>
+            <Topbar />
+            <div className="profile">
+                <Sidebar />
+                <div className="profileRight">
+                    <div className="profileRightTop">
+                        <div className="profileCover">
+                            <img
+                                className="profileCoverImg"
+                                src="assets/post/3.jpeg"
+                                alt=""
+                            />
+                            <img
+                                className="profileUserImg"
+                                src="assets/person/7.jpeg"
+                                alt=""
+                            />
+                        </div>
+                        <div className="profileInfo">
+                            <h4 className="profileInfoName">Safak Kocaoglu</h4>
+                            <span className="profileInfoDesc">Hello my friends!</span>
+                        </div>
+                    </div>
+                    <div className="profileRightBottom">
+                        <Feed />
+                        <Rightbar profile />
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
